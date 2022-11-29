55a27fcd19430fcc5c6010a8faeb0dd33a0daddf
Martin Garcia Hurtado <70974099+Martin-GarciaHurtado@users.noreply.github.com>
2022-11-29 13:29:53 -0600

    Terminacion de la parte del curso de creacion de formularios

--- CLI/form-1/src/views/HomeView.vue
+++ CLI/form-1/src/views/HomeView.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="procesarFormulario">
+    <input 
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese Nombre"
+      v-model.trim="tarea.nombre"
+    >
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        id="check-1"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="Javascript"
+      >
+      <label for="check-1" class="form-check-label">Javascript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        id="check-2"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="Node js"
+      >
+      <label for="check-2" class="form-check-label">Node.js</label>
+    </div>
+
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input 
+          type="radio"
+          id="radio-1"
+          class="form-check-input"
+          value="Urgente"
+          v-model="tarea.estado"
+        >
+          <label for="radio-1" class="form-check-label">Urgente</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          type="radio"
+          id="radio-2"
+          class="form-check-input"
+          value="Relax"
+          v-model.number="tarea.estado"
+        >
+          <label for="radio-2" class="form-check-label">Relax</label>
+      </div>
+    </div>
+    <div class="mt-2">
+      <input 
+        type="number"
+        class="form-control"
+        v-model="tarea.numero"  
+      >
+    </div>
+
+    <button 
+      type="submit"
+      class="btn btn-dark mt-2 btn-block"
+      :disabled="bloquear"
+    >
+      Procesar
+    </button>
+  </form>
+  <hr>
+  <p>{{ tarea }}</p>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tarea: {
+        nombre: '',
+        categorias: [],
+        estado: [],
+        numero: 0
+      }
+    }
+  },
+  methods: {
+    procesarFormulario(){
+      console.log(this.tarea)
+      if(this.tarea.nombre.trim() === ""){
+        console.log('Campo vacio')
+        return
+      }
+      console.log('No esta vacio el campo')
+      this.tarea = {
+        nombre: '',
+        categorias: [],
+        estado: [],
+        numero: 0
+      }
+    }
+  },
+  computed: {
+    bloquear(){
+      return this.tarea.nombre.trim() === "" ? true : false
+    }
   }
 }
 </script>
