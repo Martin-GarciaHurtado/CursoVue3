fdae9a6a7c7602dfeea04315bd35e4733dc56722
Martin Garcia Hurtado <70974099+Martin-GarciaHurtado@users.noreply.github.com>
2022-12-05 13:20:39 -0600

    Creacion de nuevo proyecto me quede pendiente en la instalacion de boostrap, vueuse/firebase

---
+++ Composition-Api/firestore-auth-1/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    HOme
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  setup() {
+
+
+    return {
+      
+
+    }
+  }
+}
+</script>
